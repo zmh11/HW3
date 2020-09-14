495f59d80d2ae9afbd3833950d670971f694a396
Zachary Hickey <zmhickey@Zacharys-MBP.local>
2020-09-14 15:51:40 -0500

    is balanced done

--- hw3.cpp
+++ hw3.cpp
@@ -1,8 +1,22 @@
 /****
 * PSEUDOCODE:
+ReverseArray:
+  int back = length of reverse
+  int temp will hold value to switch
+  for (i=0 i< half length of revers i++)
+    swap value at i with value at back 
+    back -- 
+HasBalancedParentheses
+  for the length of the string 
+    if ( open pren ++
+    if ) closed pren --
+    if closed pren > open pren 
+      return false
+  if open pren == closed pren 
+    return true
+  return false
 *
 */
-#include<iostream>
 #include <string>
 
 void ReverseArray( int reverse[], int length){
@@ -17,8 +31,23 @@ void ReverseArray( int reverse[], int length){
 	}
 }
 
-bool HasBalancedParentheses(std::string k){
-  return true;
+bool HasBalancedParentheses(std::string balance){
+  int open_pren = 0;
+  int close_pren = 0;
+
+  for( int k = 0; k < balance.length(); k++ ){
+    if(balance[k] == '(' )
+      open_pren++;
+
+    if(balance[k] == ')' )
+      close_pren++;
+      
+    if(close_pren > open_pren)
+      return false;
+  }
+  if( open_pren == close_pren )
+    return true;
+  return false;
 }
 
 bool InDogish(std::string k)
